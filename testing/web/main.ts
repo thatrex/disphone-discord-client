4592de8e03c51c1d332c888608ca38464baa593e
Rex <rex@rexslab.com>
2026-03-11 02:00:32 +0200

    added playwrite & vite for testing; changed some deps

---
+++ testing/web/main.ts
@@ -0,0 +1,10 @@
+import { GatewayIntentBits } from 'discord-api-types/v10'
+import { Client } from '../../src/'
+
+// const c = new Client({
+// 	token: import.meta.env.VITE_BOT_TOKEN,
+// 	debug: true,
+// 	intents: GatewayIntentBits.GuildVoiceStates,
+// })
+
+// c.start()
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+// https://vite.dev/config/
+
+import { defineConfig, loadEnv } from 'vite'
+import mkcert from 'vite-plugin-mkcert'
+
+const TEST_SITE_PATH = './testing/web/'
+
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, TEST_SITE_PATH, '')
+
+	if (!env.VITE_BOT_TOKEN) throw Error('Missing required environment variable: VITE_BOT_TOKEN')
+
+	return {
+		root: TEST_SITE_PATH,
+		plugins: [mkcert()],
+	}
+})
